31de821c0904bec1ee368334dd69dbc06d806bda
Thibault Palier <thibaut.palier@viacesi.fr>
2013-06-26 12:25:51 +0200

    Add formulaire validation panier

--- panier.php
+++ panier.php
@@ -1,12 +1,9 @@
-<div id="panier">
-	<h1>Panier</h1>
+
 	
-	<?php
-                
-		
-                @session_start();
-                include_once('C/ctrlPanier.php');
-                include_once('M/produit.php');
+<?php
+       @session_start();
+       include_once('C/ctrlPanier.php');
+       include_once('M/produit.php');
 		
 		/*
 		$p1 = new Produit(1, "Paul Dumont", 01, "", "", "", "2.5", "", "");
@@ -20,31 +17,34 @@
 		
 		$_SESSION['panier']->supprimer($p1);
 		//*/
+?>
 		
 		
-		
-	?>
-	
-<table>
-<?php
-			if (isset($_SESSION['login']) && $_SESSION['login'] != 'FAILED'){
-
-	foreach($_SESSION['panier']->get_items() as $item){?>
-		<form method="post" action="scripts/traitements.php">
-			<input type="hidden" name="id_form" value="3">
-				<tr>
-					<td><input type="submit" name="moins" value="-" class="buttonpanier"></td>
-					<td><?php echo $item->getQuantite();?></td>
-					<td><input type="submit" name="plus" value="+" class="buttonpanier"></td>
-					<td><?php echo $item->getProduit()->getNom();?></td>
-					<td><?php echo ($item->getProduit()->getPrix()) *  $item->getQuantite();?></td>
-					<td>€</td>
-				</tr>
-			<input type="hidden" name="id_produit" value=<?php echo $item->getProduit()->getId(); ?> >
-		</form><?php
+<div id="panier">
+	<h1>Panier</h1>	
+	<table>
+	<?php
+	if (isset($_SESSION['login']) && $_SESSION['login'] != 'FAILED'){
+		foreach($_SESSION['panier']->get_items() as $item){?>
+			<form method="post" action="scripts/traitements.php">
+				<input type="hidden" name="id_form" value="3">
+					<tr>
+						<td><input type="submit" name="moins" value="-" class="buttonpanier"></td>
+						<td><?php echo $item->getQuantite();?></td>
+						<td><input type="submit" name="plus" value="+" class="buttonpanier"></td>
+						<td><?php echo $item->getProduit()->getNom();?></td>
+						<td><?php echo ($item->getProduit()->getPrix()) *  $item->getQuantite();?></td>
+						<td>€</td>
+					</tr>
+				<input type="hidden" name="id_produit" value=<?php echo $item->getProduit()->getId(); ?> >
+			</form><?php
 	}?>
-</table>
-
+	</table>
 <b>Total  <?php echo  $_SESSION['panier']->getPrixTotal(); ?>  €</b>
 
-                        </div><?php }?>
+<form method="post" action="scripts/traitements.php">
+	<input type="hidden" name="id_form" value="3">
+	<input type="submit" name"valider" value="Valider">
+</form>
+
+</div><?php }?>
