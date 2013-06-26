1597dc4a274caaa9b8550814e5c01e3d2e7231c4
Ponk <Ponk@iPonk.local>
2013-06-26 16:32:07 +0200

    Ajout resumer panier

---
+++ resum_pharma.php
@@ -0,0 +1,24 @@
+<div class="listeproduit">
+		<table>
+			<tr>
+				<td>DÉSIGNATION</td>
+				<td>QUANTITÉ</td>
+				<td>PRIX</td>
+				<td></td>
+			</tr>
+			<?php foreach($_SESSION['panier']->get_items() as $item){?>
+			
+			<tr>
+				<td><h1><?php echo $produit->getNom()?></h1></td>
+				<td>
+					<input type="submit" name="plus" value="-" class="buttonpanier">
+					<?php echo $item->getQuantite()?>
+					<input type="submit" name="plus" value="+" class="buttonpanier">
+				</td>
+				<td><?php echo $produit->getPrix()?></td>
+				<td><a href="fiche_produit.php?id=<?php echo $produit->getId()?>" class="button">X</a></td>
+			</tr>
+			
+			<?php } ?>
+		</table>
+</div>
